e383e71f0dd18a71684f8e890ce591669dd6d484
manoj <manoj@brandingrain.com>
2026-02-14 22:33:39 +0530

    update

--- 1-basic/e1-hello.go
+++ 1-basic/e1-hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	. "fmt"
+	"strconv"
 )
 
 func main() {
@@ -12,6 +13,18 @@ func main() {
 
 	fmt.Println("Hello world")
 	Println("Hello world")
+
+	//string to int
+	a := "5"
+	b, _ := strconv.Atoi(a)
+	c := 5 + b
+	fmt.Println(c)
+
+	//int to string
+	x := 20
+	y := strconv.Itoa(x)
+	z := "x = " + y
+	fmt.Println(z)
 }
 
 //https://go.dev/play/p/oj7iN3bxnaY
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	. "fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -10,4 +12,17 @@ func main() {
 	for a, b := range os.Args {
 		Println(a, b)
 	}
+
+	//string to int
+	a := "5"
+	b, _ := strconv.Atoi(a)
+	c := 5 + b
+	fmt.Println(c)
+
+	//int to string
+	x := 20
+	y := strconv.Itoa(x)
+	z := "x = " + y
+	fmt.Println(z)
+
 }
